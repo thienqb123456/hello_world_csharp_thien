fc05303343fe5c3d165bd85f99024bc671e7d2fc
thienqb123456 <thienqb123456@gmail.com>
2022-12-06 02:00:28 +0700

    csharp_exercises_chap2_thien

---
+++ GiaiPTBac2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GiaiPTBac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number a");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number b");
+            double b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number c");
+            double c = Convert.ToInt32(Console.ReadLine());
+            double delta = b * b - 4 * a * c;
+            double x;
+           
+                if (delta < 0) {
+                    Console.WriteLine("Phuong Trinh Vo Nghiem");
+                }
+                else if (delta == 0)
+                {
+                    x = -b / 2*a;
+                    Console.WriteLine("Phuong trinh co nghiem kep : x1 = x2 = {x)", x);
+                }
+                else
+                {
+                Console.WriteLine("Phuong trinh co 2 nghiem : ");
+                    x = (-b + Math.Sqrt(delta)) / 2*a;
+                    Console.WriteLine("x = {0}", x);
+                    x = (-b - Math.Sqrt(delta)) / 2*a;
+                    Console.WriteLine("x = {0}", x) ;
+                }
+        }
+    }
+}
---
+++ csharp_exercises/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GiaiPhuongTrinhBacNhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            double b = Convert.ToInt32(Console.ReadLine());
+            double x;
+            if (a != 0)
+            {
+                x = (0 - b) / a;
+                Console.WriteLine("value of x: {0}", x);
+            } else
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Phuong Trinh co vo so nghiem!!");
+                } else
+                {
+                    Console.WriteLine("Phuong Trinh vo nghiem!!");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
