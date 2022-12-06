a3ee187c800f47d0a1d9ca60145678ddd4e0c7d3
thienqb123456 <thienqb123456@gmail.com>
2022-12-06 20:45:02 +0700

    giai thua

---
+++ TinhGiaiThua/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TinhGiaiThua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap 1 so tu nhien bat ki: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            if (a>=0)
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine("Giai thua cua so tu nhien {0} la : {1}",a, factorial);
+            } else
+            {
+                Console.WriteLine("Day khong phai so tu nhien");
+            }
+            
+        }
+    }
+}
